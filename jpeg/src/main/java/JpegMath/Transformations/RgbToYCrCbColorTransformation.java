56b91eac1f26cf98f597520521c4b295e8a0bc88
Piotr C <ppciesiolkiewicz@gmail.com>
2014-10-21 19:57:28 +0200

    first presentation added

---
+++ jpeg/src/main/java/JpegMath/Transformations/RgbToYCrCbColorTransformation.java
@@ -0,0 +1,17 @@
+package JpegMath.Transformations;
+
+import DataObjects.Tile;
+
+public class RgbToYCrCbColorTransformation implements TransformationInterface {
+
+	public Tile transfom(Tile t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Tile inverseTransform(Tile t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
