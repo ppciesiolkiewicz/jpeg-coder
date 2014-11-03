df2aa9cda532dc014e8a8b0f41bf4b0907bb87c8
Piotr C <ppciesiolkiewicz@gmail.com>
2014-11-03 14:35:11 +0100

    small changes due to Tile changes

--- jpeg/src/main/java/JpegMath/Tilers/JpegTiler.java
+++ jpeg/src/main/java/JpegMath/Tilers/JpegTiler.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Array;
 
 import DataObjects.Tile;
 
-
 //consider non-divisible by 8 arrays
 public class JpegTiler implements TilerInterface<Integer> {
 	final int tileSize = 8;
@@ -13,14 +12,14 @@ public class JpegTiler implements TilerInterface<Integer> {
 	public Tile<Integer>[][] tile(Integer[][] pixelMatrix) {
 		Integer amountY = (int) Math.ceil(pixelMatrix.length/(double) tileSize);
 		Integer amountX = (int) Math.ceil(pixelMatrix[0].length/(double) tileSize);
-		
+
 		@SuppressWarnings("unchecked")
 		Tile<Integer>[][] tiles = (Tile<Integer>[][]) Array.newInstance(Tile.class, amountY, amountX);
 		
 		for(int y = 0; y < pixelMatrix.length; y++) {
 			for(int x = 0; x < pixelMatrix[0].length; x++) {
 				if(x % tileSize == 0 && y % tileSize == 0)
-					tiles[y/8][x/8] = new Tile<Integer>(Integer.class, tileSize, tileSize);
+					tiles[y/8][x/8] = new Tile<Integer>(tileSize, tileSize, 0);
 				if(y < pixelMatrix.length && x < pixelMatrix[0].length)
 					tiles[y/8][x/8].setVal(x%tileSize, y%tileSize, pixelMatrix[y][x]);
 			}
