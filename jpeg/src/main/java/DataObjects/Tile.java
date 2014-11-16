bc29e0d0d04c0e6d713e845d4587e31a4f712d36
root <root@arch.home>
2014-11-16 15:15:47 +0100

    ZigZagIterator - not tested

--- jpeg/src/main/java/DataObjects/Tile.java
+++ jpeg/src/main/java/DataObjects/Tile.java
@@ -124,7 +124,8 @@ public class Tile<E extends Number> implements ArrayTileInterface<E> {
 		protected Tile<T> tile;
 		protected Integer curPos;
 		
-		protected static Integer[] zigZagOrder = {0,  1,  8, 16,  9,  2,  3, 10,
+		protected static Integer[] zigZagOrder = 
+				{0,  1,  8, 16,  9,  2,  3, 10,
 		         17, 24, 32, 25, 18, 11,  4,  5,
 		         12, 19, 26, 33, 40, 48, 41, 34,
 		         27, 20, 13,  6,  7, 14, 21, 28,
@@ -147,7 +148,11 @@ public class Tile<E extends Number> implements ArrayTileInterface<E> {
 		}
 
 		public void remove() {
-			tile.values.remove(curPos);
+			tile.values.remove(zigZagOrder[curPos]);
+			
+			//ensure safe remove while iterating
+			for(int i = curPos; i < zigZagOrder.length; i++)
+				--zigZagOrder[i];
 		}
 	}
 	
