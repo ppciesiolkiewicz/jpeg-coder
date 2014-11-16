c710a0bd4c792922aab6223cf82d2341bcbeed08
root <root@arch.home>
2014-11-16 14:14:09 +0100

    EncodedTile added

--- jpeg/src/main/java/DataObjects/EncodedTile.java
+++ jpeg/src/main/java/DataObjects/EncodedTile.java
@@ -1,7 +1,7 @@
 package DataObjects;
 
 public class EncodedTile<E extends Number> {
-	public Tile<E> tile;
+	public ArrayTileInterface<E> tile;
 	public Tile<Double> quantTable;
 	public HuffmanEncodingTable huffTable;
 	
