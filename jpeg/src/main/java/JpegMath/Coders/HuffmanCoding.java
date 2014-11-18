850ee9cfac50a5981c88e462d31ed4fbd8dafb76
Michal Staroszczyk <am3siak@gmail.com>
2014-11-18 15:29:41 +0100

    bug fix

--- jpeg/src/main/java/JpegMath/Coders/HuffmanCoding.java
+++ jpeg/src/main/java/JpegMath/Coders/HuffmanCoding.java
@@ -5,29 +5,43 @@ import java.util.Iterator;
 import DataObjects.Binary;
 import DataObjects.EncodedTile;
 import DataObjects.Huffman.FrequencyTable;
-import DataObjects.Huffman.HuffmanEncodingTable;
+import DataObjects.Huffman.HuffmanTable;
 import DataObjects.Huffman.HuffmanTree;
 import DataObjects.Huffman.HuffmanTreeList;
 
 public class HuffmanCoding<E extends Number> implements CodingInterface {
 
-	public void encode(EncodedTile<Integer> t) {
-		FrequencyTable<E> table = countOccurences(t);
-		HuffmanTreeList<E> list = createListOfTreeElements(table);
+	public EncodedTile<Binary> encode(EncodedTile<Integer> t) {
+		FrequencyTable<E> freqTable = countOccurences(t);
+		HuffmanTreeList<E> list = createListOfTreeElements(freqTable);
 		buildTree(list);
+		HuffmanTable<E, String> codeMap = createCodeMap(list);
+
+		return null;
 
 		// generate code for each base element
 
 		// return encoding table
 
-		t.huffTable = new HuffmanEncodingTable();
+	}
 
+	protected HuffmanTable<E, String> createCodeMap(HuffmanTreeList<E> list) {
+		HuffmanTable<E, String> codeMap = new HuffmanTable<E, String>();
+		Iterator<HuffmanTree<E>> it = list.getFirst().iterator();
+
+		while (it.hasNext()) {
+			HuffmanTree<E> elem = it.next();
+			if (elem.getValue() != null) {
+				codeMap.put(elem.getValue(), elem.getCode());
+			}
+		}
+		return codeMap;
 	}
 
 	protected void buildTree(HuffmanTreeList<E> list) {
 		// as long there are 2 elements, take the 2 smallest, create a subtree
 		// with them and readd
-		while (list.size() > 2) {
+		while (list.size() > 1) {
 			HuffmanTree<E> first = list.getSmallestWeightElement();
 			HuffmanTree<E> second = list.getSmallestWeightElement();
 
@@ -35,7 +49,8 @@ public class HuffmanCoding<E extends Number> implements CodingInterface {
 		}
 	}
 
-	protected HuffmanTreeList<E> createListOfTreeElements(FrequencyTable<E> table) {
+	protected HuffmanTreeList<E> createListOfTreeElements(
+			FrequencyTable<E> table) {
 		HuffmanTreeList<E> list = new HuffmanTreeList<E>();
 		for (E key : table.getKeys()) {
 			list.add(key, table.getWeight(key));
@@ -50,6 +65,11 @@ public class HuffmanCoding<E extends Number> implements CodingInterface {
 		while (tileIterator.hasNext())
 			table.insert(tileIterator.next());
 		return table;
+	}
+
+	public EncodedTile<Integer> decode(EncodedTile<Binary> t) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
