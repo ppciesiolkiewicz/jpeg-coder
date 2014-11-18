feaf5586d11b5b2485f5684fd994ea5f65ca3c2b
Michal Staroszczyk <am3siak@gmail.com>
2014-11-18 15:29:03 +0100

    refactoring

--- jpeg/src/main/java/DataObjects/Huffman/HuffmanTable.java
+++ jpeg/src/main/java/DataObjects/Huffman/HuffmanTable.java
@@ -3,20 +3,34 @@ package DataObjects.Huffman;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HuffmanTable<Word,Code> {
-	Map<Word,Code> wordCode = new HashMap<Word,Code>();
-	Map<Code,Word> codeWord = new HashMap<Code,Word>();
+public class HuffmanTable<Word, Code> {
+	Map<Word, Code> wordCode;
+	Map<Code, Word> codeWord; 
+
+	public HuffmanTable() {
+		wordCode = new HashMap<Word, Code>();
+		codeWord = new HashMap<Code, Word>();
+	}
 	
-	public void put(Word word,Code code){
+	public void put(Word word, Code code) {
 		wordCode.put(word, code);
 		codeWord.put(code, word);
 	}
 
-	public Code getCode (Word word){
+	public Code getCode(Word word) {
 		return wordCode.get(word);
 	}
-	
-	public Word getWord (Code code){
+
+	public Word getWord(Code code) {
 		return codeWord.get(code);
+	}
+
+	@Override
+	public String toString() {
+		String s = "";
+
+		for (Word k : wordCode.keySet())
+			s += k.toString() + " " + wordCode.get(k) + "\n";
+		return s;
 	}
 }
