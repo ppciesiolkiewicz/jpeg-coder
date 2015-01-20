b465927f30b28e84328c3cae309bf9e893b4c115
krzaczek <krzaczekw@gmail.com>
2015-01-20 23:57:06 +0100

    bug fixes

--- jpeg/src/main/java/PPM/ToPPM.java
+++ jpeg/src/main/java/PPM/ToPPM.java
@@ -1,11 +1,16 @@
 package PPM;
 
 import ImageLoader.SimpleImageLoader;
+import org.omg.CORBA.portable.OutputStream;
+import sun.nio.cs.StandardCharsets;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
 
 /**
  * Created by krzaczek on 19.01.15.
@@ -15,13 +20,13 @@ public class ToPPM {
 	public static void convert(BufferedImage bufferedImage) throws IOException {
 		int height = bufferedImage.getHeight();
 		int width = bufferedImage.getWidth();
-		PrintWriter out = new PrintWriter("/tmp/tmp.ppm");
+		PrintWriter out = new PrintWgiriter("/tmp/tmp.ppm", "UTF-8");
 		out.println("P3");
 		out.println(width + " " + height);
 		out.println("255");
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
-				printPixelARGB(bufferedImage.getRGB(i,j), out);
+				printPixelARGB(bufferedImage.getRGB(j,i), out);
 			}
 			out.println("");
 		}
